e12b4fc4ec55d535fa249552f9950c102b54d4d4
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-11-09 13:20:11 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItem = () => {
+  const elements = galleryItems
+    .map(
+      el =>
+        `<a class="gallery__item" href="${el.original}">
+  <img 
+  class="gallery__image"
+  src="${el.preview}"
+  title="${el.description}"
+  alt="${el.description}"/>
+  </a>
+  `
+    )
+    .join('');
+  gallery.innerHTML = elements;
+};
+
+galleryItem();
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
