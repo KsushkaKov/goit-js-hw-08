3caa17b408a9b26d29da49563dc073a0269ba834
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-11-09 15:59:12 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function onPlaySec({ seconds }) {
+  localStorage.setItem(CURRENT_TIME, seconds);
+}
+
+player.on('timeupdate', throttle(onPlaySec, 1000));
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
